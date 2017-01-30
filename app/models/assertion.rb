1fd7ba55b9554df688a523cd2d97e484c3675a94
Tristan Nelson <tristan@iseult.us>
2017-01-30 16:04:47 -0500

    Complete actionability score
    Actionability score (except total score) completed
    
    The performance is as now unacceptably slow, will need to rewrite for
    production display

--- app/models/assertion.rb
+++ app/models/assertion.rb
@@ -14,13 +14,27 @@ class Assertion < Entity
     {severity: "http://datamodel.clinicalgenome.org/clingen.owl#CG_000041",
      likelihood: "http://datamodel.clinicalgenome.org/clingen.owl#CG_000047",
      effectiveness: "http://datamodel.clinicalgenome.org/clingen.owl#CG_000052",
-     intervention: "http://datamodel.clinicalgenome.org/clingen.owl#CG_000086"}[category]
+     intervention: "http://datamodel.clinicalgenome.org/clingen.owl#CG_000086",
+      evidence: "http://datamodel.clinicalgenome.org/clingen.owl#CG_000042"}[category]
   end
-  
+
+  # TODO innefficient way of representing actionability scores
+  # investigate ways for making query more efficient
   def actionability_score(category)
     iri = act_cat_map(category)
-    # TODO start here and finish represenation of actionability score
-    evidence.query_as(:e).match("(e)-[:has_predicate]->(i)").where("(i)-[:subClassOf]->")
+    subscore = evidence.query_as(:e)
+                 .match("(e)-[:has_predicate]->(i)")
+                 .where("(i)-[:subClassOf]->(:RDFClass {iri: '#{iri}'})")
+                 .pluck(:i).first.score
+    
+    e = evidence.query_as(:e)
+          .match("(e)-[:has_predicate]->(i)")
+          .where("(e)-[:has_predicate]->()-[:subClassOf]->(:RDFClass {iri: '#{iri}'})")
+          .where("(i)-[:subClassOf]->(:RDFClass {iri: '#{act_cat_map(:evidence)}'})")
+          .pluck(:i).first
+
+    evidence_score = e ? e.score : ""
+    subscore + evidence_score
   end
 
 end
--- db/neo4j/migrate/20170126205849_force_create_region_uuid_constraint.rb
+++
@@ -1,9 +0,0 @@
-class ForceCreateRegionUuidConstraint < Neo4j::Migrations::Base
-  def up
-    add_constraint :region, :uuid, force: true
-  end
-
-  def down
-    drop_constraint :region, :uuid
-  end
-end
