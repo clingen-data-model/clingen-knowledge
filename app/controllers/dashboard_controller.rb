c5b117330f8e2f782c885d0dd5c6fc369b0c9d8e
Scott Goehringer <scottg@Scott.local>
2017-02-06 21:32:58 -0500

    Cleanup

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@ class DashboardController < ApplicationController
 	
 	def index
 		@agents = Agent.all 
-		@hello_message = "Welcome to JRuby on Rails on the Sun GlassFish Enterprise Server"
   end
 
 end
