7b068b3167f38551e8f33a5d749777aa70c092cc
Tristan Nelson <tristan@iseult.us>
2017-02-22 19:50:46 -0500

    Goto result if only one exists
    resolves #31

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,10 @@ class HomeController < ApplicationController
                     .limit(10)
 
     respond_to do |format|
-      format.html
+      format.html do
+        redirect_to @genes.first if @genes.length == 1 && @conditions.length == 0
+        redirect_to @conditions.first if @conditions.length == 1 && @genes.length == 0
+      end
       format.json {render json: @results, root: false}
     end
 
