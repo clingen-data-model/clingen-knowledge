c543d958714bdecec912837f827c1bb8305c160c
Scott Goehringer <scottg@Scott.local>
2017-02-23 22:36:47 -0500

    First pass at External Resources - linking items

--- app/controllers/external_resources_controller.rb
+++ app/controllers/external_resources_controller.rb
@@ -1,7 +1,22 @@
 class ExternalResourcesController < ApplicationController
+  include ActionView::Helpers::TextHelper
 
   def index
   	@gene = Gene.find_by(hgnc_id: params[:gene_id])
+  	@term_label = truncate(@gene.symbol, :length => 20, :omission => '...')
+    @term_id = truncate(@gene.hgnc_id)
+    @diseases = @gene.assertions.diseases.distinct
+    @actionability = @gene.actionability_scores
+    if @actionability.length > 0
+      @actionability_diseases = @actionability.map {|a| a[:disease]}.flatten
+    end
+    @dosage = @gene.assertions(:a).with_associations(:interpretation, :diseases)
+                .where("a:GeneDosageAssertion")
+    @validity = @gene.assertions(:n).with_associations(:interpretation, :diseases)
+                  .where("n:GeneDiseaseAssertion")
+    @diseases_detail = @validity.reduce({}) do |h, i|
+      h.update(i.diseases.reduce({}) { |h1, i1| h1.update({i1.iri => i}) })
+  end
   end
   
 end
