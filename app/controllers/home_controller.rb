463fbb21ee2af8edcbfdf0f9253940718de254b3
Tristan Nelson <tristan@iseult.us>
2017-02-28 13:37:23 -0500

    Adding regex-based search for drugs/conditions
    Resolves #28. Added rudimentary display of drug results, will need to
    elaborate on it.
    
    Search is actually more performant than expected, am impressed, this
    probably keeps us from having to use Elasticsearch for the moment.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,19 @@ class HomeController < ApplicationController
                .where("g.symbol starts with {term}")
                .params(term: t)
                .limit(10)
+
+    split_term = t.split(' ')
     
     @conditions = Condition.all(:c)
-                    .where("c.label starts with {term}")
-                    .params(term: t)
+                    .where("all(t in {terms} where c.label =~ ('(?i).*' + t + '.*'))")
+                    .params(terms: split_term)
                     .limit(10)
+
+    @drugs = Drug.all(:c)
+                    .where("all(t in {terms} where c.label =~ ('(?i).*' + t + '.*'))")
+                    .params(terms: split_term)
+                    .limit(10)
+
 
     respond_to do |format|
       format.html do
