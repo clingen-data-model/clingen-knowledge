8bbb776e20be57d90388e6cbf59aa409698a02d3
Tristan Nelson <tristan@iseult.us>
2017-03-04 17:35:54 -0500

    Removing obsolete 'curation' class'

--- app/models/actionability_intervention_curation.rb
+++
@@ -1,9 +0,0 @@
-class ActionabilityInterventionCuration < Curation
-
-  has_one :out, :curation, model_class: :ActionabilityCuration, type: :was_generated_by
-  has_one :out, :intervention, model_class: :Intervention, type: :has_object
-  has_many :in, :assertions, model_class: :ActionabilityAssertion, type: :was_generated_by
-  # Will need to add freeform 'condition' to this, possibly as a label
-  # until there are propery typed entities
-
-end
