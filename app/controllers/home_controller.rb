df8c239f9a3fa6a142d82533b4eecaf16d59da3b
Tristan Nelson <tristan@iseult.us>
2017-03-04 22:32:10 -0500

    Fixing tabs on search, fix drug display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,28 @@
 class HomeController < ApplicationController
 
   def index
-    t = params[:term]
+    @term = params[:term]
 
-    if t
-      @genes = Gene.find_by_term(t)
+    if @term
+      @genes = Gene.find_by_term(@term)
                  .limit(20)
 
-      @conditions = Condition.find_by_term(t)
+      @conditions = Condition.find_by_term(@term)
                       .limit(20)
 
-      @drugs = Drug.find_by_term(t)
+      @drugs = Drug.find_by_term(@term)
                  .limit(20)
     end
 
+    if @genes.size > 0
+      @active_class = :genes
+    elsif @conditions.size > 0
+      @active_class = :conditions
+    elsif @drugs.size > 0
+      @active_class = :drugs
+    else
+      @active_class = :none
+    end
 
     respond_to do |format|
       format.html do
