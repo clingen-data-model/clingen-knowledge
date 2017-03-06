b099f83431efe0eb1a733e7b95cf31ec7f52067b
Tristan Nelson <tristan@iseult.us>
2017-03-05 19:40:38 -0500

    Fixing indent

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,10 +1,11 @@
 class Note 
   include Neo4j::ActiveNode
 
+  property :comments
+
   has_one :out, :gene, type: :has_subject, model_class: :Gene
   has_one :out, :condition, type: :has_object, model_class: :Condition
   has_one :out, :publication, type: :was_derived_from, model_class: :Publication
-  property :comments
 
-  
+
 end
