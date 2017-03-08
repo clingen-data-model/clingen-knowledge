58764f22626f1d1d1936cd2f5151c847346e3566
Scott Goehringer <scottg@Scott.local>
2017-03-07 21:22:06 -0500

    UI/UX updates for the all curated

--- app/helpers/curations_helper.rb
+++ app/helpers/curations_helper.rb
@@ -9,6 +9,9 @@ module CurationsHelper
     end
     assertion = gene_record[:dosage].select { |a| iris.include?(a[:iri])}.first
     return "" unless assertion
-    link_to(assertion[:short_label], gene_path(id: gene_record[:hgnc_id]))
+    link_to gene_path(id: gene_record[:hgnc_id]), :class => "btn btn-success btn-xs"  do
+      "<i class=\"glyphicon glyphicon-info-sign text-white\"></i> ".html_safe + assertion[:short_label]
+    end
+    #link_to(assertion[:short_label], gene_path(id: gene_record[:hgnc_id]), class: "btn btn-success btn-xs")
   end
 end
