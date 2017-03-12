dc75ee6fbca43c21c25e623ba9b71a1cb6e21c83
Scott Goehringer <scottg@Scott.local>
2017-03-12 15:28:38 -0400

    First pass at infoButton parse

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
 
+  
+
   def index
     @term = params[:term]
+
+    if !@term
+      @term = params[:'mainSearchCriteria.v.dn']
+    end
 
     respond_to do |format|
       format.html do
