4a34676a4119d120e532a968efd3e54afeccc42e
Tristan Nelson <tristan@lappymk7.local>
2019-03-29 13:27:53 -0400

    Totally opening up cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
   allow do
-    origins 'https://*.clinicalgenome.org'
+    origins '*'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
