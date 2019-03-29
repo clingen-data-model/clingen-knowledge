6af5c6f35b1da2fb0b2d0bf59e3e2746b0478ed7
Tristan Nelson <tristan@lappymk7.local>
2019-03-29 13:15:37 -0400

    cors liberalized

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
   allow do
-    origins 'https://www.clinicalgenome.org'
+    origins 'https://*.clinicalgenome.org'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
